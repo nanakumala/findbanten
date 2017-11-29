0c237c6ccb4222774cb5575ae420f0f322f103a9
Nana Kumala <kumalanana@gmail.com>
2017-11-30 02:01:13 +0800

    change detail

--- app/src/main/java/com/example/nkumala/findbanten/DetailActivity.java
+++ app/src/main/java/com/example/nkumala/findbanten/DetailActivity.java
@@ -4,15 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nkumala.findbanten.model.TokoModel;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String KEY_TOKO="toko";
 
     TokoModel tokoModel;
+    BottomNavigationViewEx bottomNavigationViewEx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        bottomNavigationViewEx=findViewById(R.id.bnv_detail);
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
         tokoModel= (TokoModel) getIntent().getSerializableExtra(KEY_TOKO);
     }
 }
