00b5bf4b24c5e07b1b7dbd58e537bb3d5c3abb42
uripyogantara <uripyogantara@gmail.com>
2017-12-26 17:04:37 +0800

    fix map

--- app/src/main/java/com/example/nkumala/findbanten/fragment/MapsFragment.java
+++ app/src/main/java/com/example/nkumala/findbanten/fragment/MapsFragment.java
@@ -8,15 +8,47 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.nkumala.findbanten.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment{
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+    MapView mMapView;
+    private GoogleMap mMap;
     public MapsFragment() {
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_maps,container,false);
+        mMapView = (MapView) view.findViewById(R.id.map);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMapView.getMapAsync(MapsFragment.this);
         return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(-34, 151);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
 }
